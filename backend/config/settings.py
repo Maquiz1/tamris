0bcf4cd6a545c586fb0271597f299219aa9a9621
Maquiz1 <manquiz92@gmail.com>
2026-07-15 11:30:52 +0300

    Add demo.tamris.org to ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,7 +37,7 @@ DEBUG = env('DEBUG')
 # If DEBUG is True, allow all hosts. Otherwise, default to localhost to prevent crash, 
 # but expect it to be set in .env in production.
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
-ALLOWED_HOSTS += ['*', '31.97.187.66', '31.97.187.66:8080']
+ALLOWED_HOSTS += ['*', '31.97.187.66', '31.97.187.66:8080', 'demo.tamris.org']
 
 CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
 CSRF_TRUSTED_ORIGINS += [
@@ -45,6 +45,10 @@ CSRF_TRUSTED_ORIGINS += [
     'http://31.97.187.66:8080', 
     'https://31.97.187.66',
     'https://31.97.187.66:8080',
+    'http://demo.tamris.org',
+    'http://demo.tamris.org:8080',
+    'https://demo.tamris.org',
+    'https://demo.tamris.org:8080',
     'http://127.0.0.1:8000', 
     'http://localhost:8000'
 ]
